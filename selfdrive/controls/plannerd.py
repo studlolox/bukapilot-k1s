cce62b2152218a4962b02b52571bcffb62e8f717
studlolox <student910112@gmail.com>
2026-09-08 00:17:43 +0800

    feat: enable dynamic toggling of end-to-end mode in lateral planner via params

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -17,10 +17,10 @@ def plannerd_thread(sm=None, pm=None):
   CP = car.CarParams.from_bytes(params.get("CarParams", block=True))
   cloudlog.info("plannerd got CarParams: %s", CP.carName)
 
-  use_lanelines = True
+  use_lanelines = not params.get_bool('EndToEndToggle')
   wide_camera = params.get_bool('EnableWideCamera') if TICI else False
 
-  cloudlog.event("e2e mode", on=use_lanelines)
+  cloudlog.event("e2e mode", on=(not use_lanelines))
 
   longitudinal_planner = Planner(CP)
   lateral_planner = LateralPlanner(CP, use_lanelines=use_lanelines, wide_camera=wide_camera)
@@ -36,6 +36,9 @@ def plannerd_thread(sm=None, pm=None):
     sm.update()
 
     if sm.updated['modelV2']:
+      if sm.frame % 20 == 0:
+        lateral_planner.use_lanelines = not params.get_bool('EndToEndToggle')
+
       lateral_planner.update(sm)
       lateral_planner.publish(sm, pm)
       longitudinal_planner.update(sm)
